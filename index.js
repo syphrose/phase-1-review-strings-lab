109516b02b2aa29ae411177754f3b163cc31085e
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-10 01:33:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';;
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+//=>'Grace Hopper'
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${GRACE HOPPER}!'
+//=>'GRACE HOPPER'
+currentUser.slice(0,1)
+const shortGreeting = 'Welcome, ${Grace.slice(0,1)}!';
+//=>'G'
